35873041a4e1e8bc46b24325c94b10a982981598
Ismael Santiago <ismaelse95@gmail.com>
2019-10-10 09:54:23 +0200

    fichero automatizacion comentado

--- automatizacion.sh
+++ automatizacion.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+#Entrar en el entorno virtual
 cd /home/ismael
-source web_estatica/bin/activate
+source mkdocs-python/bin/activate
+
+#Crear ficheros HTML
 cd /home/ismael/GITHUB/mkdocsdesarrollo/
 git add .
 git commit -am "$1"
 git push
 mkdocs build -d ../mkdocs/
+
+#Subir a entorno de producción los ficheros HTML
 cd /home/ismael/GITHUB/mkdocs
 git add .
 git commit -am "$1"
